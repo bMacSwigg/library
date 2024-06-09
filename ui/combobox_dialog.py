812b7100810a408c756a0cb89177faebdae5682e
Brian McSwiggen <brian.mcswiggen@gmail.com>
2024-06-08 23:14:14 -0400

    Add combobox validation & cleanup

--- ui/combobox_dialog.py
+++ ui/combobox_dialog.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from tkinter.simpledialog import Dialog
 
 class ComboBoxDialog(Dialog):
@@ -7,7 +8,6 @@ class ComboBoxDialog(Dialog):
     def __init__(self, title: str, prompt: str, choices: list[str]):
         self.prompt = prompt
         self.choices = choices
-        # self.val = None  # Will hold the return value, once chosen
 
         super().__init__(None, title)
 
@@ -20,17 +20,29 @@ class ComboBoxDialog(Dialog):
         label = ttk.Label(master, text=self.prompt, justify=LEFT)
         label.grid(row=0, padx=5, sticky=W)
         
-        self.combobox = ttk.Combobox(master, value=self.choices)  # TBD: readonly?
+        self.combobox = ttk.Combobox(master, value=self.choices)
         self.combobox.grid(row=1, padx=5, sticky=W+E)
 
         return self.combobox
 
     def validate(self):
-        self.result = self.combobox.get()
+        result = self.combobox.get()
+
+        if result not in self.choices:
+            messagebox.showwarning(
+                'Unknown user',
+                'User "%s" is not in the library system' % result,
+                parent = self
+            )
+            return 0
+
+        self.result = result
         return 1
+
 
 def askcombo(title: str, prompt: str, choices: list[str]):
     return ComboBoxDialog(title, prompt, choices).result
+
 
 if __name__ == '__main__':
     print(askcombo('Test', 'What is the answer?', ['Apple', 'Banana']))
